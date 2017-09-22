63398bb744d0e7c8797a1897c6a1c7394c9782ea
musa jahun <msjahun@live.com>
2017-09-22 12:32:34 +0300

    first commit, added all files

---
+++ EMU Notification App/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace website_scraping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameRankingDataView = new System.Windows.Forms.DataGridView();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gameRankingDataView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameRankingDataView
+            // 
+            this.gameRankingDataView.AllowUserToAddRows = false;
+            this.gameRankingDataView.AllowUserToDeleteRows = false;
+            this.gameRankingDataView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.gameRankingDataView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.gameRankingDataView.BackgroundColor = System.Drawing.Color.White;
+            this.gameRankingDataView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gameRankingDataView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.gameRankingDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gameRankingDataView.ColumnHeadersVisible = false;
+            this.gameRankingDataView.GridColor = System.Drawing.SystemColors.HighlightText;
+            this.gameRankingDataView.Location = new System.Drawing.Point(24, 124);
+            this.gameRankingDataView.Name = "gameRankingDataView";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(2);
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gameRankingDataView.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.gameRankingDataView.RowHeadersVisible = false;
+            this.gameRankingDataView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            this.gameRankingDataView.Size = new System.Drawing.Size(536, 182);
+            this.gameRankingDataView.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(77, 322);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(447, 13);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "This application gets you the latest Eastern Mediteranean university student port" +
+    "al Notification";
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(91, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(377, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Announcement Notification App";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(547, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 355);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "© Copy right This piece of software was made by Musa Jahun and he reserves all th" +
+    "e rights he is ";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 368);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(332, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = " allowed to reserved he reserves the rights he is allowed to reserved. ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 383);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = " Email: msjahun@live.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(572, 405);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.gameRankingDataView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EMU Notification App ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gameRankingDataView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gameRankingDataView;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EMU Notification App/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace website_scraping
+{
+   
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        
+       
+        HtmlWeb web = new HtmlWeb();
+        public Form1()
+        {
+            InitializeComponent();
+            InitTable();
+            
+        }
+        private void InitTable()
+        {
+            table = new DataTable("GameRankingDataTable");
+            
+            table.Columns.Add("Name", typeof(string));
+           
+            gameRankingDataView.DataSource = table;
+        }
+
+        private async Task<List<NameAnd>> GameRankingFromPage()
+        {
+           string url = "https://stdportal.emu.edu.tr/";
+           // string url = "C:/Users/Musa Jahun/Desktop/Students Portal.html";
+
+
+
+            var doc = await Task.Factory.StartNew(() => web.Load(url));
+            var nameNodes = doc.DocumentNode.SelectNodes("//*[@id=\"grdwNews\"]//tr//td//table//tr//td//a");
+
+
+
+            if (nameNodes == null)
+                return new List<NameAnd>();
+
+
+            var names = nameNodes.Select(node => node.InnerText);
+           var scores = nameNodes.Select(node => node.InnerText);
+            return names.Zip(scores, (name, score) => new NameAnd() { Name = name }).ToList();
+
+        }
+
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+         
+            var rankings = await GameRankingFromPage();
+          //  while (rankings.Count < 5)
+           // {
+                foreach (var ranking in rankings)
+                    table.Rows.Add(ranking.Name);
+                
+               
+                rankings = await GameRankingFromPage();
+                
+               
+           // }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class NameAnd
+    {
+        public string Name { get; set; }
+
+    }
+}
